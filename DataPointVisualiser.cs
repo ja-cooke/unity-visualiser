1fa7c4f263c5ae4c65141d0fabf475d6d1c7c7e9
ja-cooke <89166278+ja-cooke@users.noreply.github.com>
2025-04-01 15:50:53 +0100

    Initial state of repo. Basic script for visualising a 2D audio waveform within the boundaries set by a cube. Cube meshes used for data points.

---
+++ DataPointVisualiser.cs
@@ -0,0 +1,77 @@
+using System.Runtime.Serialization;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class DataPointVisualiser : MonoBehaviour
+{
+    private const int audioBufferSize = 1024;
+    private Transform graphBoundaryT;
+    private GameObject audioObject;
+    private AudioSource audioSource;
+    private GameObject[] visualiser = new GameObject[audioBufferSize];
+    private float[] audioDataArray = new float[audioBufferSize];
+    private float[] freqDataArray = new float[audioBufferSize];
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+            audioObject = new GameObject("audioObject", typeof(AudioSource));
+            audioSource = audioObject.GetComponent<AudioSource>();
+            
+            // Load the resource for the AudioClip
+            AudioClip audioClip = Resources.Load<AudioClip>("test2");
+            // Load the resource into the AudioSource
+            audioSource.clip = audioClip;
+            audioSource.Play();
+            // Get Visualisation data
+            audioSource.GetOutputData(audioDataArray, 1);
+            audioSource.GetSpectrumData(freqDataArray, 1, FFTWindow.BlackmanHarris);
+            
+
+            // Find the Graph Boundary
+
+            graphBoundaryT = this.transform.Find("GraphBoundary");
+
+            // Instantiate cubes in the game world
+            int counter = 0;
+            foreach (float datum in audioDataArray){
+
+                // Generate a cube
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                // Set the parent as the GraphBoundary
+                cube.transform.SetParent(graphBoundaryT);
+                // Set the coordinates to local to the graph boundary
+                cube.transform.localPosition = Vector3.zero;
+                // Scale the size of each individual cube
+                cube.transform.localScale = new Vector3(0.005f,0.005f,0.005f);
+                visualiser[counter] = cube;
+                counter++;
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.GetOutputData(audioDataArray, 1);
+        audioSource.GetSpectrumData(freqDataArray, 1, FFTWindow.BlackmanHarris);
+
+        plot(audioDataArray, audioBufferSize);
+    }
+
+    private void plot(float[] dataArray, int audioBufferSize){
+        // x coordinate is depth, y coordinate is amplitude, z coordinate is time / frequency axis
+        int n = 0;
+        foreach (float datum in dataArray){
+            // 2D plot
+            float xPos = 0;
+            // Divide by 2 for a vertically centred plot of scale -0.5 <-> +0.5
+            float yPos = dataArray[n]/2;
+            // -0.5f offset for a horizontally centred plot
+            float zPos = -0.5f + (n/(float)audioBufferSize);
+
+            visualiser[n].transform.localPosition =  new Vector3(xPos,yPos,zPos);
+            n++;
+        }
+
+    }
+}
